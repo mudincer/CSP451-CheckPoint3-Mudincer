dd26f969ffaf1e9354bc348005ca1be46a92b175
Mudincer <mudincer@myseneca.ca>
2026-06-02 22:45:44 +0300

    Fix lint issue after CI failure test

--- src/app.js
+++ src/app.js
@@ -20,4 +20,3 @@ if (require.main === module) {
   const port = process.env.PORT || 3000;
   app.listen(port, () => console.log(`Listening on ${port}`));
 }
-const debugMessage = "temporary CI failure";
